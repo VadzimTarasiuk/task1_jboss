961eabaf90ef08682ec52522320da3b3c9712ced
Vadzim Tarasiuk <vad.tarasyuk@gmail.com>
2017-08-24 09:43:26 +0300

    Modify with database

--- task1_jboss/test/smoke/default/default_test.rb
+++ task1_jboss/test/smoke/default/default_test.rb
@@ -29,3 +29,16 @@ end
 describe command('curl -IL http://12.11.12.11:8080/helloworld/hi.jsp') do
   its('stdout') { should match /200 OK/ }
 end
+
+describe service('mysql-default') do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe port(3306) do
+  it { should be_listening }
+end
+
+describe command('mysql -S /var/run/mysql-default/mysqld.sock -uroot -pvagrant -e "show databases"') do
+  its('stdout') { should match /task3/ }
+end
